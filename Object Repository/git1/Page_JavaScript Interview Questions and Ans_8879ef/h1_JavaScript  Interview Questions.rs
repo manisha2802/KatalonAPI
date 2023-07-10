67ac737ac08757e233c02cd3a60572bb3e73944a
manisha.kamble <manisha.kamble@INBL01LAP30379.brd.g-c-i.com>
2023-07-10 14:29:42 +0530

    TestAgent1

---
+++ Object Repository/git1/Page_JavaScript Interview Questions and Ans_8879ef/h1_JavaScript  Interview Questions.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h1_JavaScript  Interview Questions</name>
+   <tag></tag>
+   <elementGuidId>ccdbf74e-9720-4a5f-aa5b-2004f7a29957</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h1</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='No'])[1]/following::h1[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>39b32531-53dd-4501-9bba-e17c1dcc986a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> JavaScript  Interview Questions </value>
+      <webElementGuid>04900d24-51b0-48b8-a18d-21da58e20bc8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;cannot-touch&quot;]/body[@class=&quot;tex2jax_ignore&quot;]/div[@class=&quot;ib-content&quot;]/article[@class=&quot;ibpage&quot;]/section[1]/header[@class=&quot;ibpage__header&quot;]/div[@class=&quot;ibpage__header-title&quot;]/h1[1]</value>
+      <webElementGuid>4e6f7495-d7b2-450d-b35e-ba476bfa37d6</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='No'])[1]/following::h1[1]</value>
+      <webElementGuid>ac166fc1-b738-4a4b-87cc-7ddfbe44e447</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Yes, Exit'])[1]/following::h1[1]</value>
+      <webElementGuid>ad8450c1-6c4a-45eb-b1c3-a61fb871e13e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Last updated on Dec 19, 2022'])[1]/preceding::h1[1]</value>
+      <webElementGuid>4086c1c5-3fbc-46ca-a756-56ac0f4ab7ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Download PDF'])[1]/preceding::h1[1]</value>
+      <webElementGuid>bed49fa3-12b0-4e28-96b4-b7d1adbacc50</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h1</value>
+      <webElementGuid>08b824d7-a57e-4d28-91ce-6676aba9d65d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = ' JavaScript  Interview Questions ' or . = ' JavaScript  Interview Questions ')]</value>
+      <webElementGuid>4740b41c-4389-4882-a18d-a3b639ac7bf0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/git1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>git1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a5a49075-fca7-4d49-b30c-1d4170a63add</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b51fed5-0613-4ee5-a9c3-d25cc99784be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
